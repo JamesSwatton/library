6ca1fe2381f798584a06080c2652deede1e7da46
JamesSwatton <jamessswatton@gmail.com>
2019-11-01 17:05:30 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+class Media {
+    constructor(title, isCheckedOut, ratings) {
+        this._title = title;
+        this._isCheckedOut = isCheckedOut;
+        this._ratings = ratings;
+    }
+    get title() {
+        return this._title;
+    }
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+    get ratings() {
+        return this._ratings;
+    }
+}

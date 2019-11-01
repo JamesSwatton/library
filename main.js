057e2ab0fe547e5b9dbcf8a6833970f946f550e1
JamesSwatton <jamessswatton@gmail.com>
2019-11-01 17:44:16 +0000

    add Book and Movie classes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 class Media {
-    constructor(title, isCheckedOut, ratings) {
+    constructor(title) {
         this._title = title;
-        this._isCheckedOut = isCheckedOut;
-        this._ratings = ratings;
+        this._isCheckedOut = false;
+        this._ratings = [];
     }
     get title() {
         return this._title;
@@ -24,5 +24,33 @@ class Media {
     }
     addRating(newRating) {
         this.ratings.push(newRating);
+    }
+}
+
+class Book extends Media {
+    constructor(author, title, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
+    }
+}
+
+class Movie extends Media {
+    constructor(director, title, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+    get director() {
+        return this._director;
+    }
+    get runTime() {
+        return this._runTime;
     }
 }
